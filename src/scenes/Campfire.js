e32309b009baca92268ba4d8c6edf79164864af9
mhwagoner <mhwagone@ucsc.edu>
2026-03-09 12:18:02 -0700

    yes? It's done?

--- src/scenes/Campfire.js
+++ src/scenes/Campfire.js
@@ -8,6 +8,7 @@ class Campfire extends Phaser.Scene {
         this.background = this.add.image(0, 0, 'campfire').setOrigin(0,0)
         this.background.setInteractive()
         this.background.on('pointerdown', () => {
+            console.log("Moved to walk")
             this.scene.start('walkScene')
         })
     }
